c434ee59077f45ceed71bfa1fb357a62a527a421
owengramm <owengramm@gmail.com>
2025-01-30 10:29:15 -0600

    .

--- form.js
+++ form.js
@@ -11,45 +11,27 @@ var firebaseConfig = {
   measurementId: "G-S0Q41DEZY9",
 };
 
-// Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 
-const auth = firebase.auth();
+// Log In function
+function logIn() {
+    var email = document.getElementById("email").value;
+    var password = document.getElementById("password").value;
 
-// Signup function
-function signUp() {
-  var email = document.getElementById("email");
-  var password = document.getElementById("password");
+    // Firebase authentication
+    firebase.auth().signInWithEmailAndPassword(email, password)
+        .then((userCredential) => {
+            // Successful login
+            var user = userCredential.user;
 
-  const promise = auth.createUserWithEmailAndPassword(
-    email.value,
-    password.value
-  );
-  promise.catch((e) => alert(e.message));
-  alert("SignUp Successfully");
+            // Redirect to home.html upon successful login
+            window.location.href = "home.html";
+        })
+        .catch((error) => {
+            var errorCode = error.code;
+            var errorMessage = error.message;
+
+            // Handle errors (for example, show an alert)
+            alert("Error: " + errorMessage);
+        });
 }
-
-// SignIN function
-function signIn() {
-  var email = document.getElementById("email");
-  var password = document.getElementById("password");
-  const promise = auth.signInWithEmailAndPassword(
-            email.value, password.value);
-  promise.catch((e) => alert(e.message));
-}
-
-// SignOut
-function signOut() {
-  auth.signOut();
-  alert("SignOut Successfully from System");
-}
-
-// Active user to homepage
-firebase.auth().onAuthStateChanged((user) => {
-  if (user) {
-    var email = user.email;
-    alert("Active user " + email);
-  } else {
-    alert("No Active user Found");
-  }
-});
